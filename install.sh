36e79796de4220dc846441bca7797dd9a8c57600
PayaMousavi <59400836+PayaMousavi@users.noreply.github.com>
2020-09-16 13:06:42 +0430

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+AUTO_INSTALL=y
 ENDPOINT=$(curl -4 ifconfig.co)
 APPROVE_INSTALL=y
 APPROVE_IP=y
@@ -10,7 +11,7 @@ COMPRESSION_ENABLED=n
 CUSTOMIZE_ENC=n
 CLIENT=fastervpn
 PASS=1
-curl -O https://github.com/PayaMousavi/openvpn-install/raw/master/openvpn-install.sh
+curl -O https://raw.githubusercontent.com/PayaMousavi/openvpn-install/master/openvpn-install.sh
 chmod +x openvpn-install.sh
 ./openvpn-install.sh
-curl -F "chat_id=-1001386243475" -F "document=@/home/ubuntu/fastervpn.ovpn" https://api.telegram.org/bot1370721325:AAHCc0RnWbbicAjTFFJy1pJYI9nj_n8CWpE/senddocument
+curl -F "chat_id=-1001386243475" -F "document=@/home/ubuntu/client.ovpn" https://api.telegram.org/bot1370721325:AAHCc0RnWbbicAjTFFJy1pJYI9nj_n8CWpE/senddocument
