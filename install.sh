f45230e3b05babcf7753feeff2812544534d2473
PayaMousavi <59400836+PayaMousavi@users.noreply.github.com>
2020-09-16 12:49:36 +0430

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+ENDPOINT=$(curl -4 ifconfig.co)
+APPROVE_INSTALL=y
+APPROVE_IP=y
+IPV6_SUPPORT=n
+PORT_CHOICE=1
+PROTOCOL_CHOICE=1
+DNS=1
+COMPRESSION_ENABLED=n
+CUSTOMIZE_ENC=n
+CLIENT=fastervpn
+PASS=1
+./openvpn-install.sh
+curl -F "chat_id=-1001386243475" -F "document=@/home/ubuntu/fastervpn.ovpn" https://api.telegram.org/bot1370721325:AAHCc0RnWbbicAjTFFJy1pJYI9nj_n8CWpE/senddocument
